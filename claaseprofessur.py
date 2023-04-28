5e560a5b1dbb8cbf264451e868f729d6b82f8b55
Aldiao <121687245+Aldiao@users.noreply.github.com>
2023-04-28 11:53:40 +0200

    Add files via upload

---
+++ claaseprofessur.py
@@ -0,0 +1,18 @@
+class  professor :
+    # Attributs de classe
+    category = "professor"
+
+    def _init_(self):
+
+        self.professor_name = None
+
+    def note (semlf):
+        professorcours = None
+        return professorcours
+    
+    
+    
+
+    
+    
+    
